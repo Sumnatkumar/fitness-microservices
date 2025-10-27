be090dba1ef8d6bf86d9610f7a4f4c42a975a79e
Sumant  kumar <135256285+Sumnatkumar@users.noreply.github.com>
2025-10-28 01:00:13 +0530

    fitness

---
+++ aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -0,0 +1,52 @@
+package com.fitness.aiservice.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Service
+@Slf4j
+public class GeminiService {
+
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public GeminiService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    public String getRecommendations(String details) {
+        Map<String, Object> requestBody = Map.of(
+                "model", "models/gemini-2.5-flash",
+                "contents", new Object[] {
+                       Map.of("parts", new Object[] {
+                               Map.of("text", details)
+                       })
+                }
+        );
+
+        try {
+            return webClient.post()
+                    .uri(uriBuilder -> uriBuilder
+                            .path(geminiApiUrl)
+                            .queryParam("key", geminiApiKey)
+                            .build())
+                    .header("Content-Type", "application/json")
+                    .bodyValue(requestBody)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+        } catch (Exception e) {
+            log.error("Gemini API failed ", e);
+            return "AI Recommendation temporarily unavailable.";
+        }
+    }
+}
